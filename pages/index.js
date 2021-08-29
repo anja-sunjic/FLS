4426ac6015e79f4ccd1075f63d0347171de21312
AnjaSunjic <anja.sunjic@seeba.se>
2021-08-29 23:47:31 +0200

    Init

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import About from "../components/Home/About";
 import Hero from "../components/Home/Hero";
 import Speakers from "../components/Home/Speakers";
 import Partners from "../components/Home/Partners";
+import Footer from "../components/Home/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Speakers />
       <Partners />
+      <Footer />
     </>
   );
 }
