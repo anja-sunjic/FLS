8eec8a0abeb3a0d1184fda926914d918e6be5945
AnjaSunjic <anja.sunjic@seeba.se>
2021-09-01 10:11:26 +0200

    Fix

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -3,7 +3,7 @@ export default function Hero() {
     <>
       <div className="hero">
         <div className="inner">
-          <h1>FUTURES LEADERS SUMMIT '21</h1>
+          <h1>FUTURES LEADERS SUMMIT &#39;21</h1>
           <p className="right">STEM outside the box</p>
           <p className="center">16-18 December 2021 - Tuzla, B&H</p>
           <div className="btn">
