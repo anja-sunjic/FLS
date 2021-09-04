200e76bc6d27bfc80ce64acf274319ec88f6dda9
AnjaSunjic <anja.sunjic@seeba.se>
2021-09-04 15:51:31 +0200

    Font change

--- components/Home/About.js
+++ components/Home/About.js
@@ -3,26 +3,34 @@ export default function About() {
     <>
       <section className="section about">
         <div className="container">
-          <h2>About The Event</h2>
-          <p>
-            Futures Leaders Summit gathers future STEM leaders from Bosnia and
-            Herzegovina. By establishing an event that will annually gather
-            prosperous changemakers in the field of STEM, we aim to create a
-            strong network that will change the way we perceive the industry and
-            the future of our country&#39;s development.
-            <br /> <br />
-            This year, we will welcome hundreds of STEM leaders in Tuzla. Become
-            a part of a deal-breaker story that will shape today into a new
-            tomorrow.
-          </p>
-        </div>
-        <div className="covid-line">
-          <img src="exclam.svg" alt="" />
-          <span>
-            The Organizing Committee will put in place all measures needed to
-            prevent the further spread of the COVID-19 virus. More information
-            will be published shortly.
-          </span>
+          <div className="columns">
+            <div className="column is-7">
+              <h2>About The Event</h2>
+              <p>
+                Futures Leaders Summit gathers future STEM leaders from Bosnia
+                and Herzegovina. By establishing an event that will annually
+                gather prosperous changemakers in the field of STEM, we aim to
+                create a strong network that will change the way we perceive the
+                industry and the future of our country&#39;s development.
+                <br /> <br />
+                This year, we will welcome hundreds of STEM leaders in Tuzla.
+                Become a part of a deal-breaker story that will shape today into
+                a new tomorrow.
+              </p>
+
+              <p className="covid">
+                The Organizing Committee will put in place all measures needed
+                to prevent the further spread of the COVID-19 virus. More
+                information will be published shortly.
+              </p>
+            </div>
+
+            <div className="column is-4">
+              <div className="img blur">
+                <img src="_MG_9401.jpg" alt="" />
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
