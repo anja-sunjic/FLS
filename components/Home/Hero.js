3734f21956f8935a3c07f920b4cb1a049f4b74a7
AnjaSunjic <anja.sunjic@seeba.se>
2021-09-05 13:56:10 +0200

    Colors

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -4,10 +4,14 @@ export default function Hero() {
       <div className="hero">
         <div className="inner">
           <h1>FUTURES LEADERS SUMMIT &#39;21</h1>
-          <p className="right">STEM outside the box</p>
           <p className="center">16-18 December 2021 - Tuzla, B&H</p>
-          <div className="btn _shine">
-            <span>COMING SOON</span>
+          <div className="bottom">
+            <div className="btn">
+              <span>STEM outside the box</span>
+            </div>
+            <div className="btn">
+              <span>COMING SOON</span>
+            </div>
           </div>
           <div className="hero-img blur">
             <img src="hero.jpeg" alt="" />
