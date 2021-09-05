2c81be43f38d630543f88efbec950f2e84dc83e6
AnjaSunjic <anja.sunjic@seeba.se>
2021-09-05 13:03:36 +0200

    Mobile fixes

--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,18 @@
 import MetaData from "../components/partials/seo-meta";
 import Footer from "../components/partials/Footer";
 import Header from "../components/partials/Header";
+import Decor from "../components/partials/Decor";
 import FormContainer from "../components/Contact/FormContainer";
 
 export default function Home() {
   return (
     <>
       <MetaData path="/contact" title="Contact" description="" image="" />
-      <Header />
-      <FormContainer />
+      <div className="_main">
+        <Header />
+        <FormContainer />
+        <Decor />
+      </div>
       <Footer />
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Speakers from "../components/Home/Speakers";
 import Partners from "../components/Home/Partners";
 import Footer from "../components/partials/Footer";
 import Header from "../components/partials/Header";
-import Decor from "../components/Home/Decor";
+import Decor from "../components/partials/Decor";
 
 export default function Home() {
   return (
