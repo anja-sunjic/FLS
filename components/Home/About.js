0c38f1b447ca9dafe449c2aae0e0cff8b12fe58d
karabaja2234 <selmanpatak@gmail.com>
2021-09-06 22:45:38 +0200

    Text changes - About page and Footer COVID part

--- components/Home/About.js
+++ components/Home/About.js
@@ -10,16 +10,16 @@ export default function About() {
                   About The <br /> Summit
                 </h2>
                 <p>
-                  Futures Leaders Summit gathers future STEM leaders from Bosnia
-                  and Herzegovina. By establishing an event that will annually
-                  gather prosperous changemakers in the field of STEM, we aim to
-                  create a strong network that will change the way we perceive
-                  the industry and the future of our country&#39;s development.
+                  Futures Leaders Summit gathers future STEM leaders from Bosnia and Herzegovina. 
+                  By establishing an event that will annually gather prospective change makers in the field of STEM, 
+                  we aim to create a strong network that will change the way we perceive the industry and the future 
+                  of our country&#39;s development.
                 </p>
                 <p className="accented">
-                  This year, we will welcome hundreds of STEM leaders in Tuzla.
-                  Become a part of a deal-breaker story that will shape today
-                  into a new tomorrow.
+                  This year, the big idea behind the event is the importance of interdisciplinarity in STEM, 
+                  and we will welcome STEM students and recent graduates in person in Tuzla combined with an 
+                  opportunity for numerous other enthusiasts to follow the keynote speakers online. 
+                  Become a part of a story that will shape today into a new tomorrow.
                 </p>
               </div>
             </div>
--- components/partials/Footer.js
+++ components/partials/Footer.js
@@ -10,9 +10,8 @@ export default function Footer() {
               {/* <div className="logo"></div> */}
               <p>Contact Us</p> <br />
               <p className="sub">
-                The Organizing Committee will put in place all measures needed
-                to prevent the further spread of the COVID-19 virus. More
-                information will be published shortly.
+                The organizing team will put in place all measures needed to prevent the further spread of the COVID-19 
+                virus during event days. More information will be published shortly.
               </p>
               <Link href="/contact">
                 <a className="contact-button _shine">
