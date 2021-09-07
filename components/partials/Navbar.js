955ec3eea278b0c9966d65c648366f07952d56cd
AnjaSunjic <anja.sunjic@seeba.se>
2021-09-07 13:53:35 +0200

    Hid navigation

--- components/partials/Navbar.js
+++ components/partials/Navbar.js
@@ -1,25 +1,35 @@
 export default function Navbar() {
-    return (
-      <>
-        <div>
-            <nav>
-              <div className="container">
-                <input id="nav-toggle" type="checkbox"></input>
-                  <ul className="links">
-                      <li><a href="#">Home</a></li>
-                      <li><a href="#">About</a></li>
-                      <li><a href="#">Speakers</a></li>
-                      <li><a href="#">Sponsors</a></li>
-                      <li><a href="#">Contact us</a></li>
-                  </ul>
-                  <label htmlFor="nav-toggle" className="icon-burger">
-                      <div className="line"></div>
-                      <div className="line"></div>
-                      <div className="line"></div>
-                  </label>
-              </div> 
-            </nav>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className="is-hidden">
+        <nav>
+          <div className="container">
+            <input id="nav-toggle" type="checkbox"></input>
+            <ul className="links">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Speakers</a>
+              </li>
+              <li>
+                <a href="#">Sponsors</a>
+              </li>
+              <li>
+                <a href="#">Contact us</a>
+              </li>
+            </ul>
+            <label htmlFor="nav-toggle" className="icon-burger">
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </label>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}
