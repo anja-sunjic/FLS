0697e56cd5e04d90f3715aa004e9ee4c3b48fa30
AnjaSunjic <anja.sunjic@seeba.se>
2021-09-13 20:56:17 +0200

    Hero logo

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -9,26 +9,7 @@ export default function Hero() {
           <img src="hero.jpeg" alt="" />
         </div>
         <div className="hero-content container">
-          <div className="hero-top">
-            <div className="hero-title-wrapper">
-              <span className="white">FUTURES LEADERS</span>
-              <span className="black">SUMMIT &#39;21</span>
-            </div>
-            <div className="hero-info">
-              <span>DECEMBER</span>
-              <span>Tuzla, B&H</span>
-            </div>
-          </div>
-          <div className="hero-bottom">
-            <div className="hero-button-black">
-              <span>
-                STEM outside<br></br> the box
-              </span>
-            </div>
-            <div className="hero-button-yellow">
-              <span>COMING SOON</span>
-            </div>
-          </div>
+          <img src="/hero-logo.svg" alt="" />
         </div>
       </div>
     </>
