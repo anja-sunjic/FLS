36b011883a673cbc0470f57bb9d61655b4c31e21
anja-sunjic <anja.sunjic@gmail.com>
2021-09-18 09:16:31 +0200

    Build fix

--- components/partials/seo-meta.js
+++ components/partials/seo-meta.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 const MetaData = (props) => (
   <Head>
-    <title>{props.title} | Futures Leaders Summit'21</title>
+    <title>
+      {props.title} | {`Futures Leaders Summit'21`}
+    </title>
     <meta name="description" content={props.description} />
 
     <meta property="og:type" content="website" />
