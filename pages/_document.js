760376447a672ab6f598b2fc9b98c1a0bf427571
anja-sunjic <anja.sunjic@gmail.com>
2021-10-18 00:05:29 +0200

    GA

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,24 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-EG82T2R1LC`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-EG82T2R1LC', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
