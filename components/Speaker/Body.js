4e7006815829575cde0f9b3534da4b4e1734bb0a
anja-sunjic <anja.sunjic@gmail.com>
2021-10-19 15:43:23 +0200

    Fixes

--- components/Speaker/Body.js
+++ components/Speaker/Body.js
@@ -36,7 +36,7 @@ export default function Body(props) {
               <div className="agenda-items">
                 {speakerSessions.map((x) => {
                   return (
-                    <div className="item">
+                    <div className="item" key={x.time}>
                       <div className="left">
                         <p className="time">{x.time}</p>
                         <p className="type">{x.type}</p>
@@ -44,7 +44,11 @@ export default function Body(props) {
                       <div className="_right">
                         {x.events?.map((ev) => {
                           if (ev.speakerIds.includes(props.speaker.id))
-                            return <p className="title">{ev.title}</p>;
+                            return (
+                              <p className="title" key={ev.id}>
+                                {ev.title}
+                              </p>
+                            );
                         })}
                       </div>
                     </div>
@@ -70,14 +74,12 @@ export default function Body(props) {
           <div className="sessions">{console.log(props.speaker.id)}</div>
         </div>
         <div className="hex-decor is-hidden-touch">
-          <Parallax y={[-20, 25]} x={[0, 0]} tagOuter="figure">
+          <Parallax y={[-20, 50]} x={[0, 0]} tagOuter="figure">
             <Image src="/hex.svg" alt="decor" height={1000} width={1000} />
           </Parallax>
         </div>
         <div className="hex-decor is-hidden-desktop">
-          <Parallax y={[-20, 100]} x={[0, 0]} tagOuter="figure">
-            <Image src="/hex.svg" alt="decor" height={1000} width={1000} />
-          </Parallax>
+          <Image src="/hex.svg" alt="decor" height={1000} width={1000} />
         </div>
       </section>
     </>
