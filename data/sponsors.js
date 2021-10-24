ecec9b68927ba8a59f45f7c9479c4c04501a85d5
anja-sunjic <anja.sunjic@gmail.com>
2021-10-24 20:57:29 +0200

    Footer color

---
+++ data/sponsors.js
@@ -0,0 +1,37 @@
+export const teams = [
+  {
+    name: "Organizing Committee",
+    members: [
+      {
+        name: "Haris Arnautovic",
+        title: "Chair of the Organizing Committee",
+        li: "https://www.linkedin.com/in/harisarnautovic/",
+        img: "/teams/Organizing Committee/ArnautovicHaris.jpg",
+      },
+      {
+        name: "Edhem (Eddie) Custovic",
+        title: "Organizing Committee Member",
+        li: "https://www.linkedin.com/in/ecustovic/",
+        img: "/teams/Organizing Committee/EdhemCustovic.jpg",
+      },
+      {
+        name: "Vernisa Rejhan Icindic",
+        title: "Organizing Committee Member",
+        li: "https://www.linkedin.com/in/vernisarejhan/",
+        img: "/teams/Organizing Committee/VernisaRejhanIcindic.jpg",
+      },
+      {
+        name: "Damir Mitric",
+        title: "Organizing Committee Member",
+        li: "https://www.linkedin.com/in/damirmitric/",
+        img: "/teams/Organizing Committee/DamirMitric.png",
+      },
+      {
+        name: "Haris Selmanovic",
+        title: "Organizing Committee Member",
+        li: "https://www.linkedin.com/in/harisselmanovic/ ",
+        img: "/teams/Organizing Committee/HarisSelmanovic.jpg",
+      },
+    ],
+  },
+];
