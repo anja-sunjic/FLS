e2374fb609a4467f7c0fc71571871a7f2d0d666e
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-07 19:14:25 +0100

    summary
    summary

--- data/teams.js
+++ data/teams.js
@@ -1,232 +1,227 @@
 export const teams = [
-  {
-    name: "Organizing Committee",
-    members: [
-      {
-        name: "Haris Arnautovic",
-        title: "Chair of the Organizing Committee",
-        li: "https://www.linkedin.com/in/harisarnautovic/",
-        img: "/teams/Organizing Committee/ArnautovicHaris.jpg",
-      },
-      {
-        name: "Edhem (Eddie) Custovic",
-        title: "Organizing Committee Member",
-        li: "https://www.linkedin.com/in/ecustovic/",
-        img: "/teams/Organizing Committee/EdhemCustovic.jpg",
-      },
-      {
-        name: "Vernisa Rejhan Icindic",
-        title: "Organizing Committee Member",
-        li: "https://www.linkedin.com/in/vernisarejhan/",
-        img: "/teams/Organizing Committee/VernisaRejhanIcindic.jpg",
-      },
-      {
-        name: "Damir Mitric",
-        title: "Organizing Committee Member",
-        li: "https://www.linkedin.com/in/damirmitric/",
-        img: "/teams/Organizing Committee/DamirMitric.png",
-      },
-      {
-        name: "Haris Selmanovic",
-        title: "Organizing Committee Member",
-        li: "https://www.linkedin.com/in/harisselmanovic/ ",
-        img: "/teams/Organizing Committee/HarisSelmanovic.jpg",
-      },
-    ],
-  },
-  {
-    name: "Event Coordination",
-    members: [
-      {
-        name: "Una Karabeg",
-        title: "Event Coordinator & Marketing and Branding Team Lead",
-        li: "https://www.linkedin.com/in/unakarabeg/",
-        img: "/teams/Event Coordination/UnaKarabeg.jpg",
-      },
-    ],
-  },
-  {
-    name: "Program Team",
-    members: [
-      {
-        name: "Ena Dzemila",
-        title: "Program Team Lead",
-        li: "https://www.linkedin.com/in/ena-dzemila-70093516a/",
-        img: "/teams/Program Team/EnaDzemila.jpg",
-      },
-      {
-        name: "Ivana Jeftovic",
-        title: "Program Team Member",
-        li: "https://www.linkedin.com/in/jivana/",
-        img: "/teams/Program Team/IvanaJeftovic.jpg",
-      },
-    ],
-  },
-  {
-    name: "Partnerships and Sponsorships Team",
-    members: [
-      {
-        name: "Miralem Bulic",
-        title: "Partnerships and Sponsorships Team Lead",
-        li: "https://www.linkedin.com/in/miralem-bulic/",
-        img: "/teams/Partnerships and Sponsorships Team/MiralemBulic.jpg",
-      },
-      {
-        name: "Nikola Bodulovic",
-        title: "Partnerships and Sponsorships Team Associate",
-        li: "https://www.linkedin.com/in/nikola-bodulovi%C4%87-04472b198/",
-        img: "/teams/Partnerships and Sponsorships Team/Nikola Bodulovic.jpg",
-      },
-      {
-        name: "Belma Alispahic",
-        title: "Partnerships and Sponsorships Team Member",
-        li: "https://www.linkedin.com/in/balispahic/",
-        img: "/teams/Partnerships and Sponsorships Team/BelmaAlispahic.jpg",
-      },
-      {
-        name: "Amel Helez",
-        title: "Partnerships and Sponsorships Team Member",
-        li: "https://www.linkedin.com/in/amel-helez/",
-        img: "/teams/Partnerships and Sponsorships Team/AmelHelez.jpg",
-      },
-      {
-        name: "Semir Salkic",
-        title: "Partnerships and Sponsorships Team Member",
-        li: "https://www.linkedin.com/in/salkic-semir/",
-        img: "/teams/Partnerships and Sponsorships Team/SemirSalkic.jpg",
-      },
-    ],
-  },
-  {
-    name: "Marketing and Branding Team",
-    members: [
-      {
-        name: "Valerija Eres",
-        title: "Marketing and Branding Team Member",
-        li: "https://www.linkedin.com/in/valerija-ere%C5%A1-731a4a188/",
-        img: "/teams/Marketing Team/ValerijaEres.jpg",
-      },
-      {
-        name: "Dzejla Balic",
-        title: "Marketing and Branding Team Member",
-        li: "https://www.linkedin.com/in/d%C5%BEejla-bali%C4%87-bb4870196/",
-        img: "/teams/Marketing Team/DzejlaBalic.jpg",
-      },
-      {
-        name: "Marijana Udovcic",
-        title: "Marketing and Branding Team Member/Graphic Designer",
-        li: "https://www.linkedin.com/in/marijanaudovcic/",
-        img: "/teams/Marketing Team/MarijanaUdovicic.jpg",
-      },
-      {
-        name: "Ajdin Salihovic",
-        title: "Marketing and Branding Team Member/Graphic Designer",
-        li: "https://www.linkedin.com/in/salihovic-ajdin/",
-        img: "/teams/Marketing Team/Ajdin Salihovic.jpg",
-      },
-      {
-        name: "Efnan Merdan",
-        title: "Marketing and Branding Team Member/Video Editor",
-        li: "https://www.linkedin.com/in/mefnan/",
-        img: "/teams/Marketing Team/EfnanMerdan.jpg",
-      },
-    ],
-  },
-  {
-    name: "Web Team",
-    members: [
-      {
-        name: "Anja Sunjic",
-        title: "Web Team Lead",
-        li: "https://www.linkedin.com/in/anja-sunjic/",
-        img: "/teams/Web Team/AnjaSunjic.jpeg",
-      },
-      {
-        name: "Selman Patkovic",
-        title: "Web Team Member",
-        li: "https://www.linkedin.com/in/selman-patkovic/",
-        img: "/teams/Web Team/SelmanPatkovic.jpg",
-      },
-      {
-        name: "Malik Huremovic",
-        title: "Web Team Member",
-        li: "https://www.linkedin.com/in/malikhuremovic01/",
-        img: "/teams/Web Team/MalikHuremovic.jpg",
-      },
-      {
-        name: "Ishak Kazic",
-        title: "Web Team Member",
-        li: "https://www.linkedin.com/in/ishak-kazi%C4%87-a760451a8/",
-        img: "/teams/Web Team/IshakKazic.jpg",
-      },
-      // {
-      //   name: "Zehra Zelic",
-      //   title: "Web Team Member",
-      //   li: "",
-      // },
-    ],
-  },
-  {
-    name: "Logistics Team",
-    members: [
-      {
-        name: "Selma Alicic",
-        title: "Logistics Team Lead",
-        li: "https://www.linkedin.com/in/selma-alicic/",
-        img: "/teams/Logistics Team/SelmaAlicic.jpg",
-      },
-      {
-        name: "Lejla Djapo",
-        title: "Logistics Team Member",
-        li: "https://www.linkedin.com/in/lejla-%C4%91apo/",
-        img: "/teams/Logistics Team/LejlaDjapo.jpg",
-      },
-      {
-        name: "Dijana Causevic",
-        title: "Logistics Team Member",
-        li: "https://www.linkedin.com/in/dijana-causevic/",
-        img: "/teams/Logistics Team/DijanaCausevic.jpg",
-      },
-    ],
-  },
-  {
-    name: "Technical Support Team",
-    members: [
-      {
-        name: "Emin Hajdarevic",
-        title: "Technical Support Team Lead",
-        li: "https://www.linkedin.com/in/eminleet/",
-        img: "/teams/Technical Support Team/EminHajdarevic.png",
-      },
-      {
-        name: "Zlatko Marjanovic",
-        title: "Technical Support Team Member",
-        li: "https://www.linkedin.com/in/zlatkomarjanovic/",
-        img: "/teams/Technical Support Team/ZlatkoMarjanovic.jpg",
-      },
-    ],
-  },
-  {
-    name: "Advisors",
-    members: [
-      {
-        name: "Kenan Kurdic",
-        title: "Marketing Advisor",
-        li: "https://www.linkedin.com/in/kurdic/",
-        img: "/teams/Advisors/KenanKurdic.jpg",
-      },
-      {
-        name: "Lejla Selmanovic",
-        title: "Health and Safety Advisor",
-        li: "https://www.linkedin.com/in/lejlaselmanovic/",
-        img: "/teams/Advisors/LejlaSelmanovic.jpg",
-      },
-      {
-        name: "Dario Babovic",
-        title: "Design Advisor",
-        li: "https://www.linkedin.com/in/dario-babovic-40308484/",
-        img: "/teams/Advisors/DarioBabovic.jpg",
-      },
-    ],
-  },
+	{
+		name: 'Organizing Committee',
+		members: [
+			{
+				name: 'Haris Arnautovic',
+				title: 'Chair of the Organizing Committee',
+				li: 'https://www.linkedin.com/in/harisarnautovic/',
+				img: '/teams/Organizing Committee/ArnautovicHaris.jpg',
+			},
+			{
+				name: 'Edhem (Eddie) Custovic',
+				title: 'Organizing Committee Member',
+				li: 'https://www.linkedin.com/in/ecustovic/',
+				img: '/teams/Organizing Committee/EdhemCustovic.jpg',
+			},
+			{
+				name: 'Vernisa Rejhan Icindic',
+				title: 'Organizing Committee Member',
+				li: 'https://www.linkedin.com/in/vernisarejhan/',
+				img: '/teams/Organizing Committee/VernisaRejhanIcindic.jpg',
+			},
+			{
+				name: 'Damir Mitric',
+				title: 'Organizing Committee Member',
+				li: 'https://www.linkedin.com/in/damirmitric/',
+				img: '/teams/Organizing Committee/DamirMitric.png',
+			},
+			{
+				name: 'Haris Selmanovic',
+				title: 'Organizing Committee Member',
+				li: 'https://www.linkedin.com/in/harisselmanovic/ ',
+				img: '/teams/Organizing Committee/HarisSelmanovic.jpg',
+			},
+		],
+	},
+	{
+		name: 'Event Coordination',
+		members: [
+			{
+				name: 'Una Karabeg',
+				title: 'Event Coordinator & Marketing and Branding Team Lead',
+				li: 'https://www.linkedin.com/in/unakarabeg/',
+				img: '/teams/Event Coordination/UnaKarabeg.jpg',
+			},
+		],
+	},
+	{
+		name: 'Program Team',
+		members: [
+			{
+				name: 'Ena Dzemila',
+				title: 'Program Team Lead',
+				li: 'https://www.linkedin.com/in/ena-dzemila-70093516a/',
+				img: '/teams/Program Team/EnaDzemila.jpg',
+			},
+			{
+				name: 'Ivana Jeftovic',
+				title: 'Program Team Member',
+				li: 'https://www.linkedin.com/in/jivana/',
+				img: '/teams/Program Team/IvanaJeftovic.jpg',
+			},
+		],
+	},
+	{
+		name: 'Partnerships and Sponsorships Team',
+		members: [
+			{
+				name: 'Miralem Bulic',
+				title: 'Partnerships and Sponsorships Team Lead',
+				li: 'https://www.linkedin.com/in/miralem-bulic/',
+				img: '/teams/Partnerships and Sponsorships Team/MiralemBulic.jpg',
+			},
+
+			{
+				name: 'Belma Alispahic',
+				title: 'Partnerships and Sponsorships Team Member',
+				li: 'https://www.linkedin.com/in/balispahic/',
+				img: '/teams/Partnerships and Sponsorships Team/BelmaAlispahic.jpg',
+			},
+			{
+				name: 'Amel Helez',
+				title: 'Partnerships and Sponsorships Team Member',
+				li: 'https://www.linkedin.com/in/amel-helez/',
+				img: '/teams/Partnerships and Sponsorships Team/AmelHelez.jpg',
+			},
+			{
+				name: 'Semir Salkic',
+				title: 'Partnerships and Sponsorships Team Member',
+				li: 'https://www.linkedin.com/in/salkic-semir/',
+				img: '/teams/Partnerships and Sponsorships Team/SemirSalkic.jpg',
+			},
+		],
+	},
+	{
+		name: 'Marketing and Branding Team',
+		members: [
+			{
+				name: 'Valerija Eres',
+				title: 'Marketing and Branding Team Member',
+				li: 'https://www.linkedin.com/in/valerija-ere%C5%A1-731a4a188/',
+				img: '/teams/Marketing Team/ValerijaEres.jpg',
+			},
+			{
+				name: 'Lejla Cehic',
+				title: 'Marketing and Branding Team Member',
+				li: 'https://www.linkedin.com/in/lejla-%C4%87ehi%C4%87/',
+				img: '/teams/Marketing Team/LejlaCehic.jpg',
+			},
+			{
+				name: 'Marijana Udovcic',
+				title: 'Marketing and Branding Team Member/Graphic Designer',
+				li: 'https://www.linkedin.com/in/marijanaudovcic/',
+				img: '/teams/Marketing Team/MarijanaUdovicic.jpg',
+			},
+			{
+				name: 'Ajdin Salihovic',
+				title: 'Marketing and Branding Team Member/Graphic Designer',
+				li: 'https://www.linkedin.com/in/salihovic-ajdin/',
+				img: '/teams/Marketing Team/Ajdin Salihovic.jpg',
+			},
+			{
+				name: 'Efnan Merdan',
+				title: 'Marketing and Branding Team Member/Video Editor',
+				li: 'https://www.linkedin.com/in/mefnan/',
+				img: '/teams/Marketing Team/EfnanMerdan.jpg',
+			},
+		],
+	},
+	{
+		name: 'Web Team',
+		members: [
+			{
+				name: 'Anja Sunjic',
+				title: 'Web Team Lead',
+				li: 'https://www.linkedin.com/in/anja-sunjic/',
+				img: '/teams/Web Team/AnjaSunjic.jpeg',
+			},
+			{
+				name: 'Selman Patkovic',
+				title: 'Web Team Member',
+				li: 'https://www.linkedin.com/in/selman-patkovic/',
+				img: '/teams/Web Team/SelmanPatkovic.jpg',
+			},
+			{
+				name: 'Malik Huremovic',
+				title: 'Web Team Member',
+				li: 'https://www.linkedin.com/in/malikhuremovic01/',
+				img: '/teams/Web Team/MalikHuremovic.jpg',
+			},
+			{
+				name: 'Ishak Kazic',
+				title: 'Web Team Member',
+				li: 'https://www.linkedin.com/in/ishak-kazi%C4%87-a760451a8/',
+				img: '/teams/Web Team/IshakKazic.jpg',
+			},
+			// {
+			//   name: "Zehra Zelic",
+			//   title: "Web Team Member",
+			//   li: "",
+			// },
+		],
+	},
+	{
+		name: 'Logistics Team',
+		members: [
+			{
+				name: 'Selma Alicic',
+				title: 'Logistics Team Lead',
+				li: 'https://www.linkedin.com/in/selma-alicic/',
+				img: '/teams/Logistics Team/SelmaAlicic.jpg',
+			},
+			{
+				name: 'Lejla Djapo',
+				title: 'Logistics Team Member',
+				li: 'https://www.linkedin.com/in/lejla-%C4%91apo/',
+				img: '/teams/Logistics Team/LejlaDjapo.jpg',
+			},
+			{
+				name: 'Dijana Causevic',
+				title: 'Logistics Team Member',
+				li: 'https://www.linkedin.com/in/dijana-causevic/',
+				img: '/teams/Logistics Team/DijanaCausevic.jpg',
+			},
+		],
+	},
+	{
+		name: 'Technical Support Team',
+		members: [
+			{
+				name: 'Emin Hajdarevic',
+				title: 'Technical Support Team Lead',
+				li: 'https://www.linkedin.com/in/eminleet/',
+				img: '/teams/Technical Support Team/EminHajdarevic.png',
+			},
+			{
+				name: 'Zlatko Marjanovic',
+				title: 'Technical Support Team Member',
+				li: 'https://www.linkedin.com/in/zlatkomarjanovic/',
+				img: '/teams/Technical Support Team/ZlatkoMarjanovic.jpg',
+			},
+		],
+	},
+	{
+		name: 'Advisors',
+		members: [
+			{
+				name: 'Kenan Kurdic',
+				title: 'Marketing Advisor',
+				li: 'https://www.linkedin.com/in/kurdic/',
+				img: '/teams/Advisors/KenanKurdic.jpg',
+			},
+			{
+				name: 'Lejla Selmanovic',
+				title: 'Health and Safety Advisor',
+				li: 'https://www.linkedin.com/in/lejlaselmanovic/',
+				img: '/teams/Advisors/LejlaSelmanovic.jpg',
+			},
+			{
+				name: 'Dario Babovic',
+				title: 'Design Advisor',
+				li: 'https://www.linkedin.com/in/dario-babovic-40308484/',
+				img: '/teams/Advisors/DarioBabovic.jpg',
+			},
+		],
+	},
 ];
