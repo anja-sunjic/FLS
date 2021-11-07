0fb88433803e3cefa6762e55bea621ea2e8324bb
anja-sunjic <anja.sunjic@gmail.com>
2021-11-07 17:42:38 +0100

    Fix

--- components/Home/About.js
+++ components/Home/About.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Parallax } from "react-scroll-parallax";
-import CountdownComponent from "../CountdownComponent";
+import CountdownComponent from "../Countdown/Countdown";
 export default function About() {
   return (
     <>
