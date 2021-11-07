54eb898a448b9b85980a0ffc2161976ff701a937
anja-sunjic <anja.sunjic@gmail.com>
2021-11-07 18:36:37 +0100

    Fixes

--- components/Registration/Intro.jsx
+++ components/Registration/Intro.jsx
@@ -1,34 +1,34 @@
-import Image from 'next/image';
-import heroimg from '../../public/hero.webp';
+import Image from "next/image";
+import heroimg from "../../public/hero.webp";
 
 export default function Intro() {
-	return (
-		<>
-			<div className='intro hero protocols'>
-				<div className='hero-img blur'>
-					<Image
-						src={heroimg}
-						alt='Futures Leaders Summit'
-						layout='fill'
-						priority='true'
-						placeholder='blur'
-					/>
-				</div>
-				<div className='registration_container'>
-					<h1 className='registration_heading'>
-						Join a community of future STEM leaders that are shaping their and
-						the future of Bosnia and Herzegovina !
-					</h1>
-					<h3>
-						8 Keynote Speakers <br />
-						2 Panel Discussions <br />
-						12 Workshops <br />
-						6 Company Visits <br />
-					</h3>
-					<button className='get_your_ticket_now'>Get your ticket now</button>
-					<h4>Have a discount code?</h4>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="intro hero registration">
+        <div className="hero-img blur">
+          <Image
+            src={heroimg}
+            alt="Futures Leaders Summit"
+            layout="fill"
+            priority="true"
+            placeholder="blur"
+          />
+        </div>
+        <div className="registration_container">
+          <h1 className="registration_heading">
+            Join a community of future STEM leaders that are shaping their and
+            the future of Bosnia and Herzegovina!
+          </h1>
+          <h3>
+            8 Keynote Speakers <br />
+            2 Panel Discussions <br />
+            12 Workshops <br />
+            6 Company Visits <br />
+          </h3>
+          <button className="contact-button">Get your ticket now</button>
+          <h4>Have a discount code?</h4>
+        </div>
+      </div>
+    </>
+  );
 }
