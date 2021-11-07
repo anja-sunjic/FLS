d4409b7c70f0a758ef0c75b17420d0bc0be065c6
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-07 20:27:54 +0100

    sponzori, team memberi itd..
    sponzori, team memberi itd..

--- components/Home/Partners.js
+++ components/Home/Partners.js
@@ -1,82 +1,89 @@
-import Link from "next/link";
-import PartnerLogo from "../partials/PartnerLogo";
+import Link from 'next/link';
+import PartnerLogo from '../partials/PartnerLogo';
 
 export default function Partners() {
-  return (
-    <>
-      <section className="section partners" id="partners" name="partners">
-        <div className="container">
-          <h2>Sponsors & Partners</h2>
-          <div className="top-partners">
-            <div className="partner">
-              <span className="role">Organizer</span>
-              <a
-                href="https://www.bhfuturesfoundation.org/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src="/partners/BHFF.png"
-                  alt="BH Futures Foundation"
-                  className="grayscale"
-                />
-              </a>
-            </div>
-            <div className="partner">
-              <span className="role">Platinum sponsor</span>
-              <PartnerLogo
-                url="https://htecgroup.com/"
-                img="/partners/htec.png"
-              />
-            </div>
-            <div className="partner">
-              <span className="role">Gold sponsor</span>
-              <PartnerLogo
-                url="https://www.dkr.ba/en/"
-                img="/partners/DKR.png"
-              />
-            </div>
-            <div className="partner">
-              <span className="role">Organizing Partner</span>
-              <a href="https://bhing.ba/" target="_blank" rel="noreferrer">
-                <img
-                  src="/partners/bhing.png"
-                  alt="BH ING"
-                  className="grayscale"
-                />
-              </a>
-            </div>
-            <div className="partner">
-              <span className="role">Community partners</span>
-              <div className="other">
-                <PartnerLogo
-                  url="https://bhwomen.org/en/home-eng/"
-                  img="/partners/BWBH.png"
-                />
-                <PartnerLogo
-                  url="https://www.ieee.org/"
-                  img="/partners/IEEE.png"
-                />
-                <PartnerLogo
-                  url="https://www.bhfuturesfoundation.org/innovation-nation-2021"
-                  img="/partners/IEEEBH.png"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="dots is-hidden-touch">
-            <img src="/dots.svg" alt="Futures Leaders Summit" />
-          </div>
-          <div className="cta-sec">
-            <h3>FLS&#39;21 IS LOOKING FOR SPONSORS!</h3>
-            <Link href="/contact">
-              <a className="contact-button _shine">
-                <span>Become a Sponsor</span>
-              </a>
-            </Link>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className='section partners' id='partners' name='partners'>
+				<div className='container'>
+					<h2>Sponsors & Partners</h2>
+					<div className='top-partners'>
+						<div className='partner'>
+							<span className='role'>Organizer</span>
+							<a
+								href='https://www.bhfuturesfoundation.org/'
+								target='_blank'
+								rel='noreferrer'
+							>
+								<img
+									src='/partners/BHFF.png'
+									alt='BH Futures Foundation'
+									className='grayscale'
+								/>
+							</a>
+						</div>
+						<div className='partner'>
+							<span className='role'>Platinum sponsor</span>
+							<PartnerLogo
+								url='https://htecgroup.com/'
+								img='/partners/htec.png'
+							/>
+						</div>
+						<div className='partner'>
+							<span className='role'>Gold sponsor</span>
+							<PartnerLogo
+								url='https://www.dkr.ba/en/'
+								img='/partners/DKR.png'
+							/>
+						</div>
+						<div className='partner'>
+							<span className='role'>Bronze sponsor</span>
+							<PartnerLogo
+								url='https://tehnograd-company.ba/'
+								img='/partners/TehnogradCompany.png'
+							/>
+						</div>
+						<div className='partner'>
+							<span className='role'>Organizing Partner</span>
+							<a href='https://bhing.ba/' target='_blank' rel='noreferrer'>
+								<img
+									src='/partners/bhing.png'
+									alt='BH ING'
+									className='grayscale'
+								/>
+							</a>
+						</div>
+						<div className='partner'>
+							<span className='role'>Community partners</span>
+							<div className='other'>
+								<PartnerLogo
+									url='https://bhwomen.org/en/home-eng/'
+									img='/partners/BWBH.png'
+								/>
+								<PartnerLogo
+									url='https://www.ieee.org/'
+									img='/partners/IEEE.png'
+								/>
+								<PartnerLogo
+									url='https://www.bhfuturesfoundation.org/innovation-nation-2021'
+									img='/partners/IEEEBH.png'
+								/>
+							</div>
+						</div>
+					</div>
+					<div className='dots is-hidden-touch'>
+						<img src='/dots.svg' alt='Futures Leaders Summit' />
+					</div>
+					<div className='cta-sec'>
+						<h3>FLS&#39;21 IS LOOKING FOR SPONSORS!</h3>
+						<Link href='/contact'>
+							<a className='contact-button _shine'>
+								<span>Become a Sponsor</span>
+							</a>
+						</Link>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
--- data/sponsors.js
+++ data/sponsors.js
@@ -1,62 +1,82 @@
 export const sponsors = [
-  {
-    name: "Organizer",
-    members: [
-      {
-        name: "BH Futures Foundation",
-        url: "https://www.bhfuturesfoundation.org/",
-        img: "/partners/BHFF.png",
-      },
-    ],
-  },
-  {
-    name: "Platinum sponsor",
-    members: [
-      {
-        name: "HTEC",
-        url: "https://htecgroup.com/",
-        img: "/partners/htec.png",
-      },
-    ],
-  },
-  {
-    name: "Gold sponsor",
-    members: [
-      {
-        name: "DKR",
-        url: "https://www.dkr.ba/en/",
-        img: "/partners/DKR.png",
-      },
-    ],
-  },
-  {
-    name: "Organizing Partner",
-    members: [
-      {
-        name: "BHing",
-        url: "https://bhing.ba/",
-        img: "/partners/bhing.png",
-      },
-    ],
-  },
-  {
-    name: "Community partners",
-    members: [
-      {
-        name: "bwbh",
-        url: "https://bhwomen.org/en/home-eng/",
-        img: "/partners/BWBH.png",
-      },
-      {
-        name: "ieee",
-        url: "https://www.ieee.org/",
-        img: "/partners/IEEE.png",
-      },
-      {
-        name: "ieeebh",
-        url: "https://www.bhfuturesfoundation.org/innovation-nation-2021",
-        img: "/partners/IEEEBH.png",
-      },
-    ],
-  },
+	{
+		name: 'Organizer',
+		members: [
+			{
+				name: 'BH Futures Foundation',
+				url: 'https://www.bhfuturesfoundation.org/',
+				img: '/partners/BHFF.png',
+			},
+		],
+	},
+	{
+		name: 'Platinum sponsor',
+		members: [
+			{
+				name: 'HTEC',
+				url: 'https://htecgroup.com/',
+				img: '/partners/htec.png',
+			},
+		],
+	},
+	{
+		name: 'Gold sponsor',
+		members: [
+			{
+				name: 'DKR',
+				url: 'https://www.dkr.ba/en/',
+				img: '/partners/DKR.png',
+			},
+			{
+				name: 'Porsche Bosnia and Herzegovina',
+				url: 'https://www.porschebh.ba/',
+				img: '/partners/PorscheBHallwhite.png',
+			},
+		],
+	},
+	{
+		name: 'Bronze sponsor',
+		members: [
+			{
+				name: 'Tehnograd Company',
+				url: 'https://tehnograd-company.ba/',
+				img: '/partners/TehnogradCompany.png',
+			},
+		],
+	},
+	{
+		name: 'Organizing Partner',
+		members: [
+			{
+				name: 'BHing',
+				url: 'https://bhing.ba/',
+				img: '/partners/bhing.png',
+			},
+		],
+	},
+	{
+		name: 'Community partners',
+		members: [
+			{
+				name: 'bwbh',
+				url: 'https://bhwomen.org/en/home-eng/',
+				img: '/partners/BWBH.png',
+			},
+			{
+				name: 'ieee',
+				url: 'https://www.ieee.org/',
+				img: '/partners/IEEE.png',
+			},
+			{
+				name: 'ieeebh',
+				url: 'https://www.bhfuturesfoundation.org/innovation-nation-2021',
+				img: '/partners/IEEEBH.png',
+			},
+			{
+				name: 'Softhouse',
+				url: 'https://www.softhouse.se/en/kontakt/sarajevo/',
+				img: '/partners/Softhouse.png',
+			},
+		],
+	},
 ];
