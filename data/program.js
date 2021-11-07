7a05f98dce7fb3d808c426682553275a66d2fc74
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-07 20:58:37 +0100

    Update
    Update

--- data/program.js
+++ data/program.js
@@ -1,555 +1,560 @@
 export const wed = [
-  {
-    time: "TBA",
-    type: "Workshop",
-    events: [
-      {
-        id: 1,
-        title: "Session Name TBA",
-        speakerIds: [5],
-        speakers: ["Melita Kurtović"],
-        desc: "",
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 2,
-        title: "Session Name TBA",
-        speakerIds: [6],
-        speakers: ["Amra Ibrić"],
-        desc: "",
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 3,
-        title: "Session Name TBA",
-        speakerIds: [1],
-        speakers: ["Zinka Bejtić"],
-        desc: "",
+	{
+		time: 'TBA',
+		type: 'Workshop',
+		events: [
+			{
+				id: 1,
+				title:
+					'Design the career you want: From skills to finding your North Star',
+				speakerIds: [5],
+				speakers: ['Melita Kurtović'],
+				desc: '',
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 2,
+				title:
+					'Interdisciplinary integration as a pillar of pharmaceutical industry ',
+				speakerIds: [6],
+				speakers: ['Amra Ibrić'],
+				desc: ' ',
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 3,
+				title: 'Session Name BA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 4,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: ["TBA"],
-        desc: "",
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 5,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: ["TBA"],
-        desc: "",
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 6,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: ["TBA"],
-        desc: "",
-        tags: ["F2F"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Break",
-    events: [{ title: "Travel Break" }],
-  },
-  {
-    time: "TBA",
-    type: "Ceremony",
-    events: [
-      {
-        id: 7,
-        title: "Opening Ceremony",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 4,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: ['TBA'],
+				desc: '',
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 5,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: ['TBA'],
+				desc: '',
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 6,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: ['TBA'],
+				desc: '',
+				tags: ['F2F'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Break',
+		events: [{ title: 'Travel Break' }],
+	},
+	{
+		time: 'TBA',
+		type: 'Ceremony',
+		events: [
+			{
+				id: 7,
+				title: 'Opening Ceremony',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Keynote",
-    events: [
-      {
-        id: 8,
-        title: "Session Name TBA",
-        speakerIds: [1],
-        speakers: ["Zinka Bejtić"],
-        desc: "",
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Break",
-    events: [
-      {
-        title: "Lunch Break",
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Keynote",
-    events: [
-      {
-        id: 9,
-        title: "Session Name TBA",
-        speakerIds: [4],
-        speakers: ["Mario Baroševčić"],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Keynote',
+		events: [
+			{
+				id: 8,
+				title:
+					'Take the knowledge out of the box and play with it: Creating your own educational pathway through interdisciplinary approaches and significant learning',
+				speakerIds: [1],
+				speakers: ['Zinka Bejtić'],
+				desc: '',
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Break',
+		events: [
+			{
+				title: 'Lunch Break',
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Keynote',
+		events: [
+			{
+				id: 9,
+				title:
+					'The importance of interdisciplinary thinking and skills for the future',
+				speakerIds: [4],
+				speakers: ['Mario Baroševčić'],
+				desc: '',
 
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Panel",
-    events: [
-      {
-        id: 10,
-        title: "Education & Employability",
-        speakerIds: [5],
-        speakers: ["Melita Kurtović"],
-        desc: "",
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Panel',
+		events: [
+			{
+				id: 10,
+				title: 'Education & Employability',
+				speakerIds: [5],
+				speakers: ['Melita Kurtović'],
+				desc: 'Design the career you want: From skills to finding your North Star',
 
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Social, Partner Event",
-    events: [
-      {
-        id: 11,
-        title: "Showcasing STEM Superwomen from Bosnia and Herzegovina",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Social, Partner Event',
+		events: [
+			{
+				id: 11,
+				title: 'Showcasing STEM Superwomen from Bosnia and Herzegovina',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["Invite Only"],
-        company: 1,
-      },
-    ],
-  },
+				tags: ['Invite Only'],
+				company: 1,
+			},
+		],
+	},
 ];
 
 export const thu = [
-  {
-    time: "TBA",
-    type: "Company Visit",
-    events: [
-      {
-        id: 12,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+	{
+		time: 'TBA',
+		type: 'Company Visit',
+		events: [
+			{
+				id: 12,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 13,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 13,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 14,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 14,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 15,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 15,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 16,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 16,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 17,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 17,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Break",
-    events: [{ title: "Travel Break" }],
-  },
-  {
-    time: "TBA",
-    type: "Keynote",
-    events: [
-      {
-        id: 18,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Break',
+		events: [{ title: 'Travel Break' }],
+	},
+	{
+		time: 'TBA',
+		type: 'Keynote',
+		events: [
+			{
+				id: 18,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Panel",
-    events: [
-      {
-        id: 19,
-        title: "The Versatility of Engineering - The Voice of the Industry",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Panel',
+		events: [
+			{
+				id: 19,
+				title: 'The Versatility of Engineering - The Voice of the Industry',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Break",
-    events: [
-      {
-        title: "Break",
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Keynote",
-    events: [
-      {
-        id: 20,
-        title: "Session Name TBA",
-        speakerIds: [2],
-        speakers: ["Admir Mašić"],
-        desc: "",
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Break',
+		events: [
+			{
+				title: 'Break',
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Keynote',
+		events: [
+			{
+				id: 20,
+				title: 'Session Name TBA',
+				speakerIds: [2],
+				speakers: ['Admir Mašić'],
+				desc: '',
 
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Keynote",
-    events: [
-      {
-        id: 21,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Keynote',
+		events: [
+			{
+				id: 21,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Break",
-    events: [
-      {
-        title: "Break",
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Keynote",
-    events: [
-      {
-        id: 22,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Break',
+		events: [
+			{
+				title: 'Break',
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Keynote',
+		events: [
+			{
+				id: 22,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Partner Event",
-    events: [
-      {
-        id: 23,
-        title: "IEEE Innovation Nation Finals",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Partner Event',
+		events: [
+			{
+				id: 23,
+				title: 'IEEE Innovation Nation Finals',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["Invite Only"],
-        company: 1,
-      },
-    ],
-  },
+				tags: ['Invite Only'],
+				company: 1,
+			},
+		],
+	},
 ];
 
 export const fri = [
-  {
-    time: "TBA",
-    type: "Workshop",
-    events: [
-      {
-        id: 24,
-        title: "Leading From a Growth Mindset",
-        speakerIds: [7, 8],
-        speakers: ["Anja Bundalo & Snjezana Udovicic"],
-        desc: "",
+	{
+		time: 'TBA',
+		type: 'Workshop',
+		events: [
+			{
+				id: 24,
+				title: 'Leading From a Growth Mindset',
+				speakerIds: [7, 8],
+				speakers: ['Anja Bundalo & Snjezana Udovicic'],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 25,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 25,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 26,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 26,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 27,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 27,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 28,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 28,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-      {
-        id: 29,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+			{
+				id: 29,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Break",
-    events: [{ title: "Travel Break" }],
-  },
-  {
-    time: "TBA",
-    type: "Keynote",
-    events: [
-      {
-        id: 30,
-        title: "Session Name TBA",
-        speakerIds: [3],
-        speakers: ["Feđa Delalić"],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Break',
+		events: [{ title: 'Travel Break' }],
+	},
+	{
+		time: 'TBA',
+		type: 'Keynote',
+		events: [
+			{
+				id: 30,
+				title:
+					'Chasing The Flame: Delivering Olympic mega-events in ever-changing world',
+				speakerIds: [3],
+				speakers: ['Feđa Delalić'],
+				desc: '',
 
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Keynote",
-    events: [
-      {
-        id: 31,
-        title: "Lightning Talks Powered by BH Youth -  6 Rules to Live By",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Keynote',
+		events: [
+			{
+				id: 31,
+				title: 'Lightning Talks Powered by BH Youth -  6 Rules to Live By',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-      {
-        title: "Be Entrepreneurial",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+			{
+				title: 'Be Entrepreneurial',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        company: 1,
-      },
-      {
-        title: "Strive for Excellence",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				company: 1,
+			},
+			{
+				title: 'Strive for Excellence',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        company: 1,
-      },
-      {
-        title: "Act with integrity",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				company: 1,
+			},
+			{
+				title: 'Act with integrity',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        company: 1,
-      },
-      {
-        title: "Value and embrace diversity",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				company: 1,
+			},
+			{
+				title: 'Value and embrace diversity',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        company: 1,
-      },
-      {
-        title: "Serve the community",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				company: 1,
+			},
+			{
+				title: 'Serve the community',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        company: 1,
-      },
-      {
-        title: "Partner with those those who share their vision",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				company: 1,
+			},
+			{
+				title: 'Partner with those those who share their vision',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Break",
-    events: [
-      {
-        title: "Break",
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Keynote",
-    events: [
-      {
-        id: 32,
-        title: "Session Name TBA",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Break',
+		events: [
+			{
+				title: 'Break',
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Keynote',
+		events: [
+			{
+				id: 32,
+				title: 'Session Name TBA',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F", "Online"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Ceremony",
-    events: [
-      {
-        id: 33,
-        title: "Closing Ceremony",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F', 'Online'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Ceremony',
+		events: [
+			{
+				id: 33,
+				title: 'Closing Ceremony',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["F2F"],
-        company: 1,
-      },
-    ],
-  },
-  {
-    time: "TBA",
-    type: "Social",
-    events: [
-      {
-        id: 34,
-        title: "Gala Night",
-        speakerIds: [],
-        speakers: [],
-        desc: "",
+				tags: ['F2F'],
+				company: 1,
+			},
+		],
+	},
+	{
+		time: 'TBA',
+		type: 'Social',
+		events: [
+			{
+				id: 34,
+				title: 'Gala Night',
+				speakerIds: [],
+				speakers: [],
+				desc: '',
 
-        tags: ["Invite Only"],
-        company: 1,
-      },
-    ],
-  },
+				tags: ['Invite Only'],
+				company: 1,
+			},
+		],
+	},
 ];
