104389e1e71a3c94974ef0f58de402f8b68bb819
anja-sunjic <anja.sunjic@gmail.com>
2021-11-07 19:34:30 +0100

    Changes

--- pages/registration/index.js
+++ pages/registration/index.js
@@ -1,29 +1,29 @@
-import MetaData from '../../components/partials/seo-meta';
-import Footer from '../../components/partials/Footer';
-import Header from '../../components/partials/Header';
-import Decor from '../../components/partials/Decor';
-import Intro from '../../components/Registration/Intro';
-import Rules from '../../components/Protocols/Rules';
-import Ending from '../../components/Registration/Ending';
+import MetaData from "../../components/partials/seo-meta";
+import Footer from "../../components/partials/Footer";
+import Header from "../../components/partials/Header";
+import Decor from "../../components/partials/Decor";
+import Intro from "../../components/Registration/Intro";
+import Purpose from "../../components/Registration/Purpose";
+import Ending from "../../components/Registration/Ending";
 
 export default function Registration() {
-	return (
-		<>
-			<MetaData
-				path='/registration'
-				title='Join a community of future STEM leaders that are shaping their and the future Bosnia and Herzegovina!'
-				description=''
-				image='/share.png'
-			/>
-			<div className='_main'>
-				<h1 className='is-hidden'></h1>
-				<Header />
-				<Intro />
-				<Rules />
-				<Ending />
-				<Decor />
-			</div>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <MetaData
+        path="/registration"
+        title="Registration"
+        description=""
+        image="/share.png"
+      />
+      <div className="_main">
+        <h1 className="is-hidden"></h1>
+        <Header />
+        <Intro />
+        <Purpose />
+        <Ending />
+        <Decor />
+      </div>
+      <Footer />
+    </>
+  );
 }
