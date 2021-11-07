52af3abe68ab7935a65e81738ee96d90536601db
anja-sunjic <anja.sunjic@gmail.com>
2021-11-07 19:35:45 +0100

    Fix

--- components/Registration/Ending.jsx
+++ components/Registration/Ending.jsx
@@ -19,8 +19,8 @@ export default function Ending() {
                 of a maximum Travel Grant is 250 BAM, and it covers your Summit
                 ticket, accommodation (max. three nights) and travel expenses
                 (to and from Tuzla from any town in Bosnia and Herzegovina).
-                More details about the grant's scalability you can find on the
-                Travel Grant Registration Page. Note: Bosnia and Herzegovina
+                More details about the grant&#39;s scalability you can find on
+                the Travel Grant Registration Page. Note: Bosnia and Herzegovina
                 Futures Foundation Senior Scholars and selected volunteers
                 automatically receive the travel grant based on their
                 scholarships and active engagement as volunteers.
