13c2a4e059cbe30b29d922ba23edb9c3112cf657
Ali Mokayes <ali@experfy.com>
2021-11-14 20:24:03 +0100

    add link on 'here' to protocol page, vertical center the health and safety text

--- components/Registration/Purpose.js
+++ components/Registration/Purpose.js
@@ -4,20 +4,24 @@ export default function Purpose() {
       <div className="purpose registration">
         <div className="container">
           <div className="_content">
-            <p>
-              All in person attendees will be required to follow the guidelines
-              of our health and safety protocol. For in person participation it
-              is mandatory to provide one of the following during all three days
-              of the event: evidence of full vaccination; or a valid negative
-              PCR or antigen test not older than 48h; or proof of COVID-19
-              recovery issued by your doctor not older than 6 months. For those
-              attendees who would need to do the test, the FLS Organizing Team
-              will provide advice and guidelines. <br /> <br /> Read more about
-              FLS&#39;21 Health and Safety Protocols here.
-            </p>
-            <h2>
-              FLS&#39;21 HEALTH AND <br /> SAFETY PROTOCOLS
-            </h2>
+            <div>
+              <p>
+                All in person attendees will be required to follow the guidelines
+                of our health and safety protocol. For in person participation it
+                is mandatory to provide one of the following during all three days
+                of the event: evidence of full vaccination; or a valid negative
+                PCR or antigen test not older than 48h; or proof of COVID-19
+                recovery issued by your doctor not older than 6 months. For those
+                attendees who would need to do the test, the FLS Organizing Team
+                will provide advice and guidelines. <br /> <br /> Read more about
+                FLS&#39;21 Health and Safety Protocols <a href="/protocols">here</a>.
+              </p>
+            </div>
+            <div className="health-safety-center">
+              <h2>
+                FLS&#39;21 HEALTH AND <br /> SAFETY PROTOCOLS
+              </h2>
+            </div>
           </div>
         </div>
       </div>
