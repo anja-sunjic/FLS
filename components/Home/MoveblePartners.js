69935d5403029f0622bd48f36e43a4f6fa7ddf93
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-23 00:43:37 +0100

    Traka - v1
    Traka - v1

---
+++ components/Home/MoveblePartners.js
@@ -0,0 +1,37 @@
+import HorizontalScroll from 'react-horizontal-scrolling';
+const partnerImages = [
+	{ url: '/partners/BHFF.png' },
+	{ url: '/partners/htec.png' },
+	{ url: '/partners/DKR.png' },
+	{ url: '/partners/PorscheBHallwhite.png' },
+	{ url: '/partners/Mistral.png' },
+	{ url: '/partners/TehnogradCompany.png' },
+	{ url: '/partners/bhing.png' },
+	{ url: '/partners/BWBH.png' },
+	{ url: '/partners/IEEE.png' },
+	{ url: '/partners/IEEEBH.png' },
+	{ url: '/partners/Softhouse.png' },
+	{ url: '/partners/LiderLab.png' },
+	{ url: '/partners/untz.png' },
+];
+
+export default function Partners() {
+	return (
+		<div>
+			<HorizontalScroll scrollWithArrows={true} hideScrollbar={true}>
+				{partnerImages.map((item, index) => (
+					<img
+						style={{
+							height: '15rem',
+							verticalAlign: 'center',
+							margin: '10rem',
+						}}
+						src={item.url}
+						key={index}
+						alt={item.url}
+					/>
+				))}
+			</HorizontalScroll>
+		</div>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,51 @@
-import MetaData from "../components/partials/seo-meta";
-import About from "../components/Home/About";
-import Hero from "../components/Home/Hero";
-import Speakers from "../components/Home/Speakers";
-import Partners from "../components/Home/Partners";
-import Footer from "../components/partials/Footer";
-import Header from "../components/partials/Header";
-import Decor from "../components/partials/Decor";
-import CTAsection from "../components/partials/cta-section";
+import MetaData from '../components/partials/seo-meta';
+import About from '../components/Home/About';
+import Hero from '../components/Home/Hero';
+import Speakers from '../components/Home/Speakers';
+import MoveblePartners from '../components/Home/MoveblePartners';
+import Footer from '../components/partials/Footer';
+import Header from '../components/partials/Header';
+import Decor from '../components/partials/Decor';
+import CTAsection from '../components/partials/cta-section';
 //fix
 export default function Home() {
-  return (
-    <>
-      <MetaData
-        path="/"
-        title="Home"
-        description="Futures Leaders Summit (FLS) gathers future STEM leaders from
+	return (
+		<>
+			<MetaData
+				path='/'
+				title='Home'
+				description="Futures Leaders Summit (FLS) gathers future STEM leaders from
                   Bosnia and Herzegovina. We aim to create a strong network that will change the
                   way we perceive the industry and the future of our
                   country's development."
-        image="/share.png"
-      />
-      <div className="_main home">
-        <h1 className="is-hidden">Futures Leaders Summit</h1>
-        <Header />
+				image='/share.png'
+			/>
+			<div className='_main home'>
+				<h1 className='is-hidden'>Futures Leaders Summit</h1>
+				<Header />
 
-        <Hero />
-        <About />
-        <CTAsection
-          button="See Schedule"
-          title="discover this year’s program"
-          url="/schedule"
-        />
-        <Speakers />
-        <Partners />
-        {/* <CTAsection
+				<Hero />
+				<About />
+				<CTAsection
+					button='See Schedule'
+					title='discover this year’s program'
+					url='/schedule'
+				/>
+				<Speakers />
+				<MoveblePartners />
+				{/* <CTAsection
           button="Learn more"
           title="fls ‘21 health & safety protocols"
           url="/protocols"
         /> */}
-        <CTAsection
-          button="Get Tickets"
-          title="Don't miss this opportunity!"
-          url="/registration"
-        />
-        <Decor />
-      </div>
-      <Footer />
-    </>
-  );
+				<CTAsection
+					button='Get Tickets'
+					title="Don't miss this opportunity!"
+					url='/registration'
+				/>
+				<Decor />
+			</div>
+			<Footer />
+		</>
+	);
 }
