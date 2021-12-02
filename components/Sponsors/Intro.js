42e603841e4c6d72ae39be46944827d8eee7f29b
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-12-02 21:34:45 +0100

    Taskovi
    Taskovi

--- components/Sponsors/Intro.js
+++ components/Sponsors/Intro.js
@@ -1,31 +1,31 @@
-import Image from "next/image";
-import heroimg from "../../public/hero.webp";
+import Image from 'next/image';
+import heroimg from '../../public/hero.webp';
 export default function Intro() {
-  return (
-    <>
-      <div className="team hero small-hero">
-        <div className="hero-img blur">
-          <Image
-            src={heroimg}
-            alt="Futures Leaders Summit"
-            layout="fill"
-            priority="true"
-            placeholder="blur"
-            quality={60}
-            loading="eager"
-          />
-        </div>
-        <div className="container">
-          <div className="inner">
-            <div className="title-line">
-              <h2>Sponsors and Partners</h2>
-              <div className="line-decor is-hidden-touch">
-                <img src="/dots.svg" alt="Futures Leaders Summit" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='team hero small-hero'>
+				<div className='hero-img blur'>
+					<Image
+						src={heroimg}
+						alt='Futures Leaders Summit'
+						layout='fill'
+						priority='true'
+						placeholder='blur'
+						quality={60}
+						loading='eager'
+					/>
+				</div>
+				<div className='container'>
+					<div className='inner'>
+						<div className='title-line'>
+							<h2>Sponsors and Partners</h2>
+							<div className='line-decor is-hidden-touch'>
+								<img src='/dots.svg' alt='Futures Leaders Summit' />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
--- components/partials/PartnerLogo.js
+++ components/partials/PartnerLogo.js
@@ -1,47 +1,61 @@
 /* eslint-disable @next/next/no-img-element */
-import { Image } from 'next/dist/client/image';
+
 export default function PartnerLogo({ url, img, size, name }) {
+	console.log(name);
 	function SortOfDynamicStyling() {
-		if (name === 'ieeebh') {
-			return (
-				<img
-					src={img}
-					alt='Futures Leaders Summit'
-					style={{ height: '10rem', marginTop: '-3rem' }}
-				/>
-			);
-		} else if (name === 'ZenDev') {
-			return (
-				<img
-					src={img}
-					alt='Futures Leaders Summit'
-					style={{ height: '3rem', margin: '0' }}
-				/>
-			);
-		} else if (name === 'Softhouse') {
-			return (
-				<img
-					src={img}
-					alt='Futures Leaders Summit'
-					style={{ height: '3.5rem' }}
-				/>
-			);
-		} else if (name === 'Tehnograd Company') {
-			return (
-				<img
-					src={img}
-					alt='Futures Leaders Summit'
-					style={{ height: '10rem' }}
-				/>
-			);
-		} else {
-			return (
-				<img
-					src={img}
-					alt='Futures Leaders Summit'
-					style={size ? { height: `${size}` } : { height: `5rem` }}
-				/>
-			);
+		switch (name) {
+			case 'DKR':
+				return <img src={img} alt='FLS' style={{ height: '4rem' }} />;
+			case 'Porsche Bosnia and Herzegovina':
+				return <img src={img} alt='FLS' style={{ height: '4rem' }} />;
+			case 'EESTEC LC Tuzla':
+				return (
+					<img
+						src={img}
+						alt='FLS'
+						style={{ height: '8rem', marginTop: '-2rem' }}
+					/>
+				);
+			case 'ieeebh':
+				return (
+					<img
+						src={img}
+						alt='Futures Leaders Summit'
+						style={{ height: '10rem', marginTop: '-3rem' }}
+					/>
+				);
+			case 'ZenDev':
+				return (
+					<img
+						src={img}
+						alt='Futures Leaders Summit'
+						style={{ height: '3rem', margin: '0' }}
+					/>
+				);
+			case 'Softhouse':
+				return (
+					<img
+						src={img}
+						alt='Futures Leaders Summit'
+						style={{ height: '3.5rem' }}
+					/>
+				);
+			case 'Tehnograd Company':
+				return (
+					<img
+						src={img}
+						alt='Futures Leaders Summit'
+						style={{ height: '10rem' }}
+					/>
+				);
+			default:
+				return (
+					<img
+						src={img}
+						alt='Futures Leaders Summit'
+						style={size ? { height: `${size}` } : { height: `5rem` }}
+					/>
+				);
 		}
 	}
 
