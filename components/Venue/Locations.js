ba140fc5eea0d964b552cf3b2c5128f2444d18b4
farisbegic <faris.begic@stu.ssst.edu.ba>
2021-12-02 19:13:30 +0100

    Created Venue Page

---
+++ components/Venue/Locations.js
@@ -0,0 +1,19 @@
+export default function Locations() {
+    return (
+        <>
+            <div className="purpose">
+                <div className="container">
+                    <div className="venue-content">
+                        <p>
+                            All in person attendees will be required to follow the guidelines of our health and safety protocol. For in person participation it is mandatory to provide one of the following during all three days of the event: evidence of full vaccination; or a valid negative PCR or antigen test not older than 48h; or proof of COVID-19 recovery issued by your doctor not older than 6 months. For those attendees who would need to do the test, the FLS Organizing Team will provide advice and guidelines.
+                            <br/>
+                            <br/>
+                            Read more about FLS'21 Health and Safety Protocols here.
+                        </p>
+                        <h2 className="venue-heading">Workshop, Tech Tutorial and Company Visit locations</h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ pages/venue.js
@@ -0,0 +1,24 @@
+import Footer from '../components/partials/Footer';
+import Header from '../components/partials/Header';
+import Map from '../components/Venue/Map';
+import Intro from "../components/Venue/Intro";
+import Decor from "../components/partials/Decor";
+import Locations from "../components/Venue/Locations";
+import Parking from "../components/Venue/Parking";
+
+export default function Home() {
+    return (
+        <>
+            <div className='_main'>
+                <h1 className='is-hidden'>About Futures Leaders Summit</h1>
+                <Header />
+                <Map />
+                <Intro />
+                <Locations />
+                <Parking />
+                <Decor />
+            </div>
+            <Footer />
+        </>
+    );
+}
