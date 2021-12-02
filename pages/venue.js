64165c0f85e0e3cf986b2bb2f42b2a8b8f34d0e3
farisbegic <faris.begic@stu.ssst.edu.ba>
2021-12-02 21:44:51 +0100

    Added everything, waiting for logs

--- pages/venue.js
+++ pages/venue.js
@@ -1,6 +1,7 @@
 import Footer from '../components/partials/Footer';
 import Header from '../components/partials/Header';
 import Map from '../components/Venue/Map';
+import Intro from "../components/Venue/Intro";
 import Decor from "../components/partials/Decor";
 import Locations from "../components/Venue/Locations";
 import Parking from "../components/Venue/Parking";
@@ -12,6 +13,7 @@ export default function Venue() {
                 <h1 className='is-hidden'>About Futures Leaders Summit</h1>
                 <Header />
                 <Map />
+                <Intro />
                 <Locations />
                 <Parking />
                 <Decor />
