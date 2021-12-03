06a644fc9987361b430331b03cea42b4874952a8
farisbegic <faris.begic@stu.ssst.edu.ba>
2021-12-03 10:03:54 +0100

    Added norefferer

--- components/Venue/Locations.js
+++ components/Venue/Locations.js
@@ -10,19 +10,19 @@ export default function Locations() {
                             <br/>
                             <br/>
                             Workshops will be held at the &nbsp;
-                            <a href="https://goo.gl/maps/F95t92WZSx8aTFqQA" target="_blank" className="link-black-bold-underline">Faculty of Mechanical Engineering Tuzla</a>, &nbsp;
-                            <a href="https://goo.gl/maps/L489eNjJDsDTmG61A" target="_blank" className="link-black-bold-underline">Law School Tuzla</a>, &nbsp;
-                            <a href="https://goo.gl/maps/QrMZ3SnLYie9oLRu5" target="_blank" className="link-black-bold-underline">BIT Centar Tuzla</a>, &nbsp;
-                            <a href="https://goo.gl/maps/Hs8F7GJTMRScVs416" target="_blank" className="link-black-bold-underline">IPI Academy</a>, and &nbsp;
-                            <a href="https://goo.gl/maps/fNuC7tXqCcrngb5x9" target="_blank" className="link-black-bold-underline">FINra</a>.
+                            <a href="https://goo.gl/maps/F95t92WZSx8aTFqQA" target="_blank" rel="noreferrer" className="link-black-bold-underline">Faculty of Mechanical Engineering Tuzla</a>, &nbsp;
+                            <a href="https://goo.gl/maps/L489eNjJDsDTmG61A" target="_blank" rel="noreferrer" className="link-black-bold-underline">Law School Tuzla</a>, &nbsp;
+                            <a href="https://goo.gl/maps/QrMZ3SnLYie9oLRu5" target="_blank" rel="noreferrer" className="link-black-bold-underline">BIT Centar Tuzla</a>, &nbsp;
+                            <a href="https://goo.gl/maps/Hs8F7GJTMRScVs416" target="_blank" rel="noreferrer" className="link-black-bold-underline">IPI Academy</a>, and &nbsp;
+                            <a href="https://goo.gl/maps/fNuC7tXqCcrngb5x9" target="_blank" rel="noreferrer" className="link-black-bold-underline">FINra</a>.
                             <br/>
                             <br/>
                             Tech tutorials or workshops will be held at &nbsp;
-                            <a href="https://goo.gl/maps/7C3KbNXM1DZjJJiu5" target="_blank" className="link-black-bold-underline">FINra powered by HTEC</a>, &nbsp;
-                            <a href="https://g.page/dkr-tuzla?share" target="_blank" className="link-black-bold-underline">DRK - German Center for Robotics</a>, &nbsp;
-                            <a href="https://goo.gl/maps/EEaY1eaQuaXBXvHM6" target="_blank" className="link-black-bold-underline">TEHNOGRAD-company</a>, &nbsp;
-                            <a href="https://goo.gl/maps/Ft5YeV22WLNY6rVf6" target="_blank" className="link-black-bold-underline">Tuzla Thermal Power Plant</a>, and the &nbsp;
-                            <a href="https://goo.gl/maps/F95t92WZSx8aTFqQA" target="_blank" className="link-black-bold-underline">Faculty of Mechanical Engineering Tuzla</a>, &nbsp;
+                            <a href="https://goo.gl/maps/7C3KbNXM1DZjJJiu5" target="_blank" rel="noreferrer" className="link-black-bold-underline">FINra powered by HTEC</a>, &nbsp;
+                            <a href="https://g.page/dkr-tuzla?share" target="_blank" rel="noreferrer" className="link-black-bold-underline">DRK - German Center for Robotics</a>, &nbsp;
+                            <a href="https://goo.gl/maps/EEaY1eaQuaXBXvHM6" target="_blank" rel="noreferrer" className="link-black-bold-underline">TEHNOGRAD-company</a>, &nbsp;
+                            <a href="https://goo.gl/maps/Ft5YeV22WLNY6rVf6" target="_blank" rel="noreferrer" className="link-black-bold-underline">Tuzla Thermal Power Plant</a>, and the &nbsp;
+                            <a href="https://goo.gl/maps/F95t92WZSx8aTFqQA" target="_blank" rel="noreferrer" className="link-black-bold-underline">Faculty of Mechanical Engineering Tuzla</a>, &nbsp;
                         </p>
                         <h2 className="venue-heading">Workshop, Tech Tutorial and Company Visit locations</h2>
                     </div>
