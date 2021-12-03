2a65419af352147bb5ac4c1a8f19502aa44f778c
farisbegic <faris.begic@stu.ssst.edu.ba>
2021-12-03 06:57:00 +0100

    Changed text on venue page

--- components/Venue/Locations.js
+++ components/Venue/Locations.js
@@ -5,10 +5,24 @@ export default function Locations() {
                 <div className="container">
                     <div className="venue-content">
                         <p>
-                            All in person attendees will be required to follow the guidelines of our health and safety protocol. For in person participation it is mandatory to provide one of the following during all three days of the event: evidence of full vaccination; or a valid negative PCR or antigen test not older than 48h; or proof of COVID-19 recovery issued by your doctor not older than 6 months. For those attendees who would need to do the test, the FLS Organizing Team will provide advice and guidelines.
+                            Workshops, tech tutorials and/or company visits will happen at various locations all across Tuzla.
+                            The goal  is to familiarize FLS’21 participants with the Tuzla region and its major organizations and institutions.
                             <br/>
                             <br/>
-                            Read more about FLS'21 Health and Safety Protocols here.
+                            Workshops will be held at the &nbsp;
+                            <a href="https://goo.gl/maps/F95t92WZSx8aTFqQA" target="_blank" className="link-black-bold-underline">Faculty of Mechanical Engineering Tuzla</a>, &nbsp;
+                            <a href="https://goo.gl/maps/L489eNjJDsDTmG61A" target="_blank" className="link-black-bold-underline">Law School Tuzla</a>, &nbsp;
+                            <a href="https://goo.gl/maps/QrMZ3SnLYie9oLRu5" target="_blank" className="link-black-bold-underline">BIT Centar Tuzla</a>, &nbsp;
+                            <a href="https://goo.gl/maps/Hs8F7GJTMRScVs416" target="_blank" className="link-black-bold-underline">IPI Academy</a>, and &nbsp;
+                            <a href="https://goo.gl/maps/fNuC7tXqCcrngb5x9" target="_blank" className="link-black-bold-underline">FINra</a>.
+                            <br/>
+                            <br/>
+                            Tech tutorials or workshops will be held at &nbsp;
+                            <a href="https://goo.gl/maps/7C3KbNXM1DZjJJiu5" target="_blank" className="link-black-bold-underline">FINra powered by HTEC</a>, &nbsp;
+                            <a href="https://g.page/dkr-tuzla?share" target="_blank" className="link-black-bold-underline">DRK - German Center for Robotics</a>, &nbsp;
+                            <a href="https://goo.gl/maps/EEaY1eaQuaXBXvHM6" target="_blank" className="link-black-bold-underline">TEHNOGRAD-company</a>, &nbsp;
+                            <a href="https://goo.gl/maps/Ft5YeV22WLNY6rVf6" target="_blank" className="link-black-bold-underline">Tuzla Thermal Power Plant</a>, and the &nbsp;
+                            <a href="https://goo.gl/maps/F95t92WZSx8aTFqQA" target="_blank" className="link-black-bold-underline">Faculty of Mechanical Engineering Tuzla</a>, &nbsp;
                         </p>
                         <h2 className="venue-heading">Workshop, Tech Tutorial and Company Visit locations</h2>
                     </div>
