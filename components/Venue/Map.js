8e4bc533abe405eb7f9d700450afb93f85e50dba
farisbegic <faris.begic@stu.ssst.edu.ba>
2021-12-04 11:26:46 +0100

    Test

--- components/Venue/Map.js
+++ components/Venue/Map.js
@@ -4,7 +4,7 @@ const Map = () => {
     return (
         <>
             <iframe className="venue-location"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2844.1102262577033!2d18.65225561552655!3d44.53337557910098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475ead0dc56a517d%3A0x3f389c2bfedbefbf!2sBosnian%20Cultural%20Center%20of%20Tuzla%20Canton!5e0!3m2!1shr!2sba!4v1638459375010!5m2!1shr!2sba"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2844.1102262577033!2d18.65225561552655!3d44.53337557910098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475ead0dc56a517d%3A0x3f389c2bfedbefbf!2sBosnian%20Cultural%20Center%20of%20Tuzla%20Canton!5e0!3m2!1shr!2sba!4v1638459375010!5m2!1shr!2sba"
             />
         </>
     );
