f7a54c4c109651b936d0fde1d23cda86f009f56e
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-12-09 14:46:50 +0100

    aa
    aa

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,44 @@
-import { ParallaxProvider } from "react-scroll-parallax";
-import "../styles/main.scss";
-import "../public/DatePicker.css";
-import "../public/Calendar.css";
-import Head from "next/head";
+import { ParallaxProvider } from 'react-scroll-parallax';
+import '../styles/main.scss';
+import '../public/DatePicker.css';
+import '../public/Calendar.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-      </Head>
-      <ParallaxProvider>
-        <Component {...pageProps} />
-      </ParallaxProvider>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<link rel='shortcut icon' href='/favicon.png' />
+				<link rel='manifest' href='/site.webmanifest' />
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon-16x16.png'
+				/>
+			</Head>
+			<ParallaxProvider>
+				<Component {...pageProps} />
+			</ParallaxProvider>
+		</>
+	);
 }
 
 export default MyApp;
