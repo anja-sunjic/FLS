0ae15ec497cca3413aae61b98b7b2c730c80788c
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-12-09 14:47:04 +0100

    aa
    aa

--- pages/_app.js
+++ pages/_app.js
@@ -21,12 +21,7 @@ function MyApp({ Component, pageProps }) {
 					sizes='32x32'
 					href='/favicon-32x32.png'
 				/>
-				<link
-					rel='icon'
-					type='image/png'
-					sizes='32x32'
-					href='/favicon-32x32.png'
-				/>
+
 				<link
 					rel='icon'
 					type='image/png'
