8711e0b0beb5557d6aa561dd32f1ba092b99c594
farisbegic <faris.begic@stu.ssst.edu.ba>
2021-12-10 06:44:08 +0100

    Rebase

---
+++ pages/accommodation-and-food.js
@@ -0,0 +1,22 @@
+import MetaData from '../components/partials/seo-meta';
+import Footer from '../components/partials/Footer';
+import Header from '../components/partials/Header';
+import Decor from '../components/partials/Decor';
+import Intro from '../components/Accommodation/Intro';
+import RecommendedHotels from '../components/Accommodation/RecommendedHotels';
+
+export default function Home() {
+    return (
+        <>
+            <MetaData path='/accommodation-and-food' title='Accommodation & Food' description='' image='/share.png' />
+            <div className='_main speakers'>
+                <h1 className='is-hidden'>Accommodation, Food and Refreshments Futures Leaders Summit</h1>
+                <Header />
+                <Intro />
+                <RecommendedHotels />
+                <Decor />
+            </div>
+            <Footer />
+        </>
+    );
+}
